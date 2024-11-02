3cde567ad8676ddc530596882252884d0bf0e658
dennis <dennisliu010906@gmail.com>
2024-11-02 16:36:22 -0400

    started landing page

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1></h1>
+      <h1
+        className="relative w-[max-content] font-mono before:absolute before:inset-0 before:animate-typewriter before:bg-white after:absolute after:inset-0 after:w-[0.125em] after:animate-caret after:bg-black"
+      >
+        StarHub
+      </h1>
     </div>
   );
 }
